1354af371f3d83f484ce3144d3470ec678c2c1a5
gwoot43 <77466931+gwoot43@users.noreply.github.com>
2021-03-24 23:13:00 +1100

    Update hash.py

--- hash.py
+++ hash.py
@@ -28,12 +28,13 @@ while True:
     with open('confession_real.txt','rb') as f:
         bytes_real = f.read() 
         hash_real = hashlib.sha256(bytes_real).hexdigest()
-    if hash_fake[-8:] != hash_real[-8:]:
+    if hash_fake[-5:] != hash_real[-5:]:
         with open ('confession_fake.txt','a') as f:
             f.write(' ')
-        with open ('confession_real.txt','a') as f:
-            f.write(' ')
             continue
+    else:
+        print('Done!')               
+
 
 
 
