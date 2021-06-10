831485c8ee0805eaa4b2beb3695b889ec24b86ad
gwoot43 <77466931+gwoot43@users.noreply.github.com>
2021-06-10 19:49:13 +1000

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,14 +1,14 @@
 import hashlib
-
+Birthday attack attempt
 while True:
-    with open('filename','rb') as f:
+    with open('filename1','rb') as f:
         bytes_fake = f.read() 
         hash_fake = hashlib.sha256(bytes_fake).hexdigest()
-    with open('filename','rb') as f:
+    with open('filename2','rb') as f:
         bytes_real = f.read() 
         hash_real = hashlib.sha256(bytes_real).hexdigest()
     if hash_fake[-2:] != hash_real[-2:]:
-        with open ('filename','a') as f:
+        with open ('filename2','a') as f:
             f.write(' ')
             continue
     else:
